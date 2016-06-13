afcc4b34cd3dafc9b85afbbcf5e2e991159a3af8
cptran777 <cptran14@gmail.com>
2016-06-13 15:26:17 -0700

    Blinky Dancer inherits from Dancer

--- src/blinkyDancer.js
+++ src/blinkyDancer.js
@@ -1,15 +1,17 @@
 var BlinkyDancer = function(top, left, timeBetweenSteps) {
   Dancer.call(this);
+  this.step();
 };
 
 BlinkyDancer.prototype = Object.create(Dancer.prototype);
 BlinkyDancer.prototype.constructor = BlinkyDancer;
 
 BlinkyDancer.prototype.step = function() {
+  var self = this;
   // call the old version of step at the beginning of any call to this new version of step
-  Dancer.prototype.step.call(this);
+  Dancer.prototype.step.call(self);
   // toggle() is a jQuery method to show/hide the <span> tag.
   // See http://api.jquery.com/category/effects/ for this and
   // other effects you can use on a jQuery-wrapped html tag.
-  this.$node.toggle();
+  self.$node.toggle();
 };
--- src/dancer.js
+++ src/dancer.js
@@ -3,16 +3,19 @@ var Dancer = function(top, left, timeBetweenSteps) {
 
   // use jQuery to create an HTML <span> tag
   this.$node = $('<span class="dancer"></span>');
+  //this.step();
+  this.setPosition(top, left);
+  this.timeBetweenSteps = timeBetweenSteps;
 
   // now that we have defined the dancer object, we can start setting up important parts of it by calling the methods we wrote
   // this one sets the position to some random default point within the body
-
 };
 
 Dancer.prototype.step = function() {
     // the basic dancer doesn't do anything interesting at all on each step,
     // it just schedules the next step
-  setTimeout(this.step, timeBetweenSteps);
+  var self = this;
+  setTimeout(Dancer.prototype.step.bind(self), self.timeBetweenSteps);
 };
 
 Dancer.prototype.setPosition = function(top, left) {
